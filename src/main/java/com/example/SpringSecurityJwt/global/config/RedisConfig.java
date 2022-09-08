425b3b1cfde3fc0f95ad461a911a138b606c59a0
ecvheo1 <ecvheo@naver.com>
2022-09-09 01:42:58 +0900

    :bulb: redis 설정 주석 추가

--- src/main/java/com/example/SpringSecurityJwt/global/config/RedisConfig.java
+++ src/main/java/com/example/SpringSecurityJwt/global/config/RedisConfig.java
@@ -29,6 +29,10 @@ public class RedisConfig {
   @Bean
   public RedisTemplate<String, String> redisTemplate() {
     RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+    /**
+     * setKeySerializer, setValueSerializer 설정
+     * redis-cli을 통해 직접 데이터를 조회 시 알아볼 수 없는 형태로 출력되는 것을 방지
+     */
     redisTemplate.setKeySerializer(new StringRedisSerializer());
     redisTemplate.setValueSerializer(new StringRedisSerializer());
     redisTemplate.setConnectionFactory(redisConnectionFactory());
