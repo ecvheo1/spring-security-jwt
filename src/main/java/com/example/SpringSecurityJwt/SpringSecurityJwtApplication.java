b5d1f79674b2f1373f04d94cad0a5f19e122d933
ecvheo1 <ecvheo@naver.com>
2022-09-09 01:23:47 +0900

    :sparkles: redis 설정 추가

--- src/main/java/com/example/SpringSecurityJwt/SpringSecurityJwtApplication.java
+++ src/main/java/com/example/SpringSecurityJwt/SpringSecurityJwtApplication.java
@@ -5,9 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 import com.example.SpringSecurityJwt.global.config.JwtConfig;
+import com.example.SpringSecurityJwt.global.config.RedisConfig;
 
 @SpringBootApplication
-@EnableConfigurationProperties(JwtConfig.class)
+@EnableConfigurationProperties({
+		JwtConfig.class,
+		RedisConfig.class
+})
 public class SpringSecurityJwtApplication {
 
 	public static void main(String[] args) {
